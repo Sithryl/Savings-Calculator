54b97b276652fde2103a00976e92f5af9255bae6
Sithryl <david@tgsummit.com>
2023-06-12 16:45:33 -0400

    set up file structure and added navbar

---
+++ savings-calculator/app/layout.jsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Nav from '../../components/Nav';
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Savings Calculator",
+  description: "Find out how much money you could save",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Nav/>
+        {children}
+      </body>
+    </html>
+  );
+}
--- savings-calculator/tailwind.config.js
+++ savings-calculator/tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'light-blue': '#3aafa9',
+      'darker-blue': '#2b7a78',
+      'amber': '#fcb900',
+      'black': '#17252a',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
